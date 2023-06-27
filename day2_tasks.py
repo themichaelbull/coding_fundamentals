cbb169edef9fb363b4e5598ae87d62c08544aaad
themichaelbull <contactmichaelbull@gmail.com>
2023-06-27 11:19:53 +0100

    Added another task for day 2

--- day2_tasks.py
+++ day2_tasks.py
@@ -6,9 +6,23 @@ student_score = int(input("Hi there! What was your score from the exam today?\n"
 
 if student_score >= 85:
     print("You got a distinction. Well done!")
-elif student_score >= 64:
+elif student_score >= 65:
     print("You got a pass. Nice!")
 else:
     print("I'm sorry to say you failed.")
 
 ### ### ###
+
+# Task 2 - Weight Converter
+
+pound_terms = ("lb", "lbs", "pounds")
+kg_terms = ("kg", "kgs", "kilograms")
+
+user_weight, weight_type = (input("What is your weight and what measurement is it in? Please seperate these by a space: ")).split()
+
+if weight_type.lower() in pound_terms:
+    print(round(int(user_weight) * 0.45359237,1), "pounds")
+elif weight_type.lower() in kg_terms:
+    print(int(user_weight) * 2.2, "kg")
+else:
+    print("You didn't provide a weight or weight type correctly. Please run the program again.")
